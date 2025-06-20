e54b9847dbc7539cd0d7baff065bb4473926b4d3
Kiran Kannur <kiran.kannur@fortanix.com>
2025-06-20 17:03:59 +0530

    job trait

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,17 @@
-use chrono::{DateTime, Datelike, Duration, NaiveTime, Utc, Weekday};
+use chrono::{DateTime, Datelike, Duration, NaiveTime, Timelike, Utc, Weekday};
 use std::sync::Arc;
 
 #[derive(Clone, Debug)]
 pub enum TimeUnit {
+    Seconds,
+    Minutes,
     Hours,
     Days,
-    Minutes,
-    Seconds,
     Weeks,
+}
+
+pub trait Scheduler {
+    fn run_if_due(&mut self, now: DateTime<Utc>);
 }
 
 pub struct Job {
@@ -20,23 +24,84 @@ pub struct Job {
     remaining_runs: Option<i32>,
 }
 
-pub struct Scheduler {
-    jobs: Vec<Job>,
+impl Scheduler for Job {
+    fn run_if_due(&mut self, now: DateTime<Utc>) {
+        if let Some(0) = self.remaining_runs {
+            return;
+        }
+
+        if let Some(wanted_day) = self.weekday {
+            if now.weekday() != wanted_day {
+                return;
+            }
+        }
+
+        let should_run = match self.last_run {
+            None => true,
+            Some(last) => {
+                let elapsed = now - last;
+                let interval = match self.time_unit {
+                    TimeUnit::Seconds => Duration::seconds(self.interval as i64),
+                    TimeUnit::Minutes => Duration::minutes(self.interval as i64),
+                    TimeUnit::Hours => Duration::hours(self.interval as i64),
+                    TimeUnit::Days => Duration::days(self.interval as i64),
+                    TimeUnit::Weeks => Duration::weeks(self.interval as i64),
+                };
+                elapsed >= interval
+            }
+        };
+
+        if should_run {
+            if let Some(at_time) = self.at_time {
+                if now.time() < at_time {
+                    return;
+                }
+
+                if let Some(last_run) = self.last_run {
+                    match self.time_unit {
+                        TimeUnit::Days | TimeUnit::Weeks => {
+                            if last_run.date_naive() == now.date_naive() {
+                                return;
+                            }
+                        }
+                        _ => {}
+                    }
+                }
+            }
+
+            (self.task)();
+            self.last_run = Some(now);
+            if let Some(ref mut count) = self.remaining_runs {
+                *count -= 1;
+            }
+        }
+    }
 }
 
-impl Scheduler {
+pub struct JobRunner {
+    jobs: Vec<Box<dyn Scheduler>>,
+}
+
+impl JobRunner {
     pub fn new() -> Self {
-        Scheduler { jobs: vec![] }
+        JobRunner { jobs: vec![] }
     }
 
     pub fn every(&mut self, interval: u64) -> JobBuilder {
         JobBuilder {
             interval,
-            scheduler: self,
+            job_runner: self,
             time_unit: None,
             at_time: None,
             weekday: None,
             repeat: None,
+        }
+    }
+
+    pub fn run_pending(&mut self) {
+        let now = Utc::now();
+        for job in &mut self.jobs {
+            job.run_if_due(now);
         }
     }
 }
@@ -45,7 +110,7 @@ pub struct JobBuilder<'a> {
     interval: u64,
     time_unit: Option<TimeUnit>,
     at_time: Option<NaiveTime>,
-    scheduler: &'a mut Scheduler,
+    job_runner: &'a mut JobRunner,
     weekday: Option<Weekday>,
     repeat: Option<i32>,
 }
@@ -55,22 +120,18 @@ impl<'a> JobBuilder<'a> {
         self.time_unit = Some(TimeUnit::Seconds);
         self
     }
-
     pub fn minutes(mut self) -> Self {
         self.time_unit = Some(TimeUnit::Minutes);
         self
     }
-
-    pub fn days(mut self) -> Self {
-        self.time_unit = Some(TimeUnit::Days);
-        self
-    }
-
     pub fn hours(mut self) -> Self {
         self.time_unit = Some(TimeUnit::Hours);
         self
     }
-
+    pub fn days(mut self) -> Self {
+        self.time_unit = Some(TimeUnit::Days);
+        self
+    }
     pub fn week(mut self) -> Self {
         self.time_unit = Some(TimeUnit::Weeks);
         self
@@ -85,36 +146,31 @@ impl<'a> JobBuilder<'a> {
         self.weekday = Some(Weekday::Mon);
         self
     }
-
     pub fn tuesday(mut self) -> Self {
         self.weekday = Some(Weekday::Tue);
         self
     }
-
     pub fn wednesday(mut self) -> Self {
         self.weekday = Some(Weekday::Wed);
         self
     }
-
     pub fn thursday(mut self) -> Self {
         self.weekday = Some(Weekday::Thu);
         self
     }
-
     pub fn friday(mut self) -> Self {
         self.weekday = Some(Weekday::Fri);
         self
     }
-
     pub fn saturday(mut self) -> Self {
         self.weekday = Some(Weekday::Sat);
         self
     }
-
     pub fn sunday(mut self) -> Self {
         self.weekday = Some(Weekday::Sun);
         self
     }
+
     pub fn repeat(mut self, count: i32) -> Self {
         self.repeat = Some(count);
         self
@@ -126,102 +182,29 @@ impl<'a> JobBuilder<'a> {
     {
         let job = Job {
             interval: self.interval,
-            time_unit: self.time_unit.unwrap(),
+            time_unit: self.time_unit.expect("TimeUnit required"),
             at_time: self.at_time,
             task: Arc::new(job_fn),
             last_run: None,
             weekday: self.weekday,
             remaining_runs: self.repeat,
         };
-        self.scheduler.jobs.push(job);
-    }
-}
 
-impl Scheduler {
-    pub fn run_pending(&mut self) {
-        let now = Utc::now();
-        for job in &mut self.jobs {
-            if let Some(0) = job.remaining_runs {
-                continue;
-            }
-            if let Some(wanted_day) = job.weekday {
-                if now.weekday() != wanted_day {
-                    continue;
-                }
-            }
-
-            let should_run = match job.last_run {
-                None => true,
-                Some(last) => {
-                    let elapsed = now - last;
-                    let interval = match job.time_unit {
-                        TimeUnit::Seconds => Duration::seconds(job.interval as i64),
-                        TimeUnit::Minutes => Duration::minutes(job.interval as i64),
-                        TimeUnit::Hours => Duration::hours(job.interval as i64),
-                        TimeUnit::Days => Duration::days(job.interval as i64),
-                        TimeUnit::Weeks => Duration::weeks(job.interval as i64),
-                    };
-                    elapsed >= interval
-                }
-            };
-
-            if should_run {
-                if let Some(at_time) = job.at_time {
-                    if now.time() < at_time {
-                        continue;
-                    }
-
-                    if let Some(last_run) = job.last_run {
-                        match job.time_unit {
-                            TimeUnit::Days | TimeUnit::Weeks => {
-                                if last_run.date_naive() == now.date_naive() {
-                                    continue;
-                                }
-                            }
-                            _ => {}
-                        }
-                    }
-                }
-
-                (job.task)();
-                job.last_run = Some(now);
-                if let Some(ref mut count) = job.remaining_runs {
-                    *count -= 1;
-                }
-            }
-        }
+        self.job_runner.jobs.push(Box::new(job));
     }
 }
 
 fn main() {
-    let mut scheduler = Scheduler::new();
+    let mut runner = JobRunner::new();
 
-    //scheduler
-    //    .every(3)
-    //    .seconds()
-    //    .do_(|| println!("task scheduled"));
-
-    //scheduler
-    //    .every(1)
-    //    .days()
-    //    .at("19:24")
-    //    .do_(|| println!("task scheduled"));
-    //
-    //scheduler
-    //    .every(1)
-    //    .week()
-    //    .tuesday()
-    //    .at("19:24")
-    //    .do_(|| println!("task scheduled"));
-    //
-    scheduler
+    runner
         .every(3)
         .seconds()
         .repeat(3)
         .do_(|| println!("task scheduled"));
 
     loop {
-        scheduler.run_pending();
+        runner.run_pending();
         std::thread::sleep(std::time::Duration::from_secs(1));
     }
 }
